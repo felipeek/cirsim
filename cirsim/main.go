c8d5d95ae8902e10c8d4ae7f095ee0c03f07fcd8
Felipeek <felipeek@hotmail.com>
2018-04-20 22:40:35 -0300

    Program args

--- cirsim/main.go
+++ cirsim/main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"os"
 
 	"github.com/felipeek/cirsim/internal"
 )
 
 func main() {
-	internal.ParserInit(os.Args[1])
-	//internal.ParserInit("./res/example.spice")
+	var filePath string
+	flag.StringVar(&filePath, "path", "", "Spice file path")
+	flag.Parse()
+
+	if filePath == "" {
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	_, err := os.Stat(filePath)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: Spice file does not exist")
+		os.Exit(1)
+	}
+
+	internal.ParserInit(filePath)
 }
