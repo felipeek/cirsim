d163841b226f8d9c28ed86dca5e78906457b7327
Felipeek <felipeek@hotmail.com>
2018-05-21 23:56:21 -0300

    LU

--- internal/mna.go
+++ internal/mna.go
@@ -2,11 +2,17 @@ package internal
 
 import (
 	"fmt"
+	"math"
 	"os"
 )
 
 func mnaSolveLinear(elementList *Element, nodesMap map[string]int) {
-	elementListPrint(elementList)
+	//elementListPrint(elementList)
+
+	//H1 := [][]float64{{1, 0, 2}, {2, 1, 0}, {2, 1, 1}}
+	//B1 := []float64{1, 2, 1}
+
+	//mnaLUFactorization(H1, B1)
 
 	// identify groups
 	currentElement := elementList
@@ -43,16 +49,6 @@ func mnaSolveLinear(elementList *Element, nodesMap map[string]int) {
 		currentElement = currentElement.Next
 	}
 
-	fmt.Println("NodesMap:")
-	for k, v := range nodesMap {
-		fmt.Printf("%+v: %+v\n", k, v)
-	}
-
-	fmt.Println("CurrentMap:")
-	for k, v := range currentNodes {
-		fmt.Printf("%+v: %+v\n", k, v)
-	}
-
 	// Create H Matrix
 	H := make([][]float64, len(nodesMap)+len(currentNodes)-1)
 	for i, _ := range H {
@@ -63,8 +59,10 @@ func mnaSolveLinear(elementList *Element, nodesMap map[string]int) {
 	B := make([]float64, len(nodesMap)+len(currentNodes)-1)
 
 	mnaBuildMatrices(elementList, currentNodes, H, B)
+	X := mnaLUFactorization(H, B)
 
-	fmt.Printf("H:\n\n%+v\n\nB:\n\n%+v\n\n", H, B)
+	//fmt.Println("X: %+v\n", X)
+	mnaPrintMatrices(H, B, X)
 }
 
 func mnaBuildMatrices(elementList *Element, currentNodes map[string]int, H [][]float64, B []float64) {
@@ -219,5 +217,96 @@ func mnaBuildMatrices(elementList *Element, currentNodes map[string]int, H [][]f
 		}
 
 		e = e.Next
+	}
+}
+
+func mnaLUFactorization(H [][]float64, B []float64) []float64 {
+	LU := make([][]float64, len(H))
+
+	for i := 0; i < len(H); i++ {
+		LU[i] = make([]float64, len(H))
+
+		for j := 0; j < len(H); j++ {
+			LU[i][j] = H[i][j]
+		}
+	}
+	for k := 0; k < len(H); k++ {
+
+		// partial pivoting
+		absValue := math.Abs(LU[k][k])
+		absIndex := k
+		for i := k + 1; i < len(H); i++ {
+			if math.Abs(LU[k][i]) > absValue {
+				absIndex = i
+				absValue = math.Abs(LU[k][i])
+			}
+		}
+
+		//switch rows
+		tmp := LU[k]
+		LU[k] = LU[absIndex]
+		LU[absIndex] = tmp
+
+		tmp2 := B[k]
+		B[k] = B[absIndex]
+		B[absIndex] = tmp2
+
+		for i := k + 1; i < len(H); i++ {
+			LU[i][k] = H[i][k] / H[k][k]
+			for j := k + 1; j < len(H); j++ {
+				LU[i][j] = H[i][j] - H[i][k]*H[k][j]
+			}
+		}
+	}
+
+	//printMatrix(LU)
+
+	Y := make([]float64, len(B))
+	for k := 0; k < len(H); k++ {
+		Y[k] = B[k]
+		for j := 0; j < k-1; j++ {
+			Y[k] = Y[k] - LU[k][j]*Y[j]
+		}
+		Y[k] = Y[k] / LU[k][k]
+	}
+
+	X := make([]float64, len(B))
+	for k := len(H) - 1; k >= 0; k-- {
+		X[k] = B[k]
+		for j := k + 1; j < len(H); j++ {
+			X[k] = X[k] - LU[k][j]*X[j]
+		}
+		X[k] = X[k] / LU[k][k]
+	}
+
+	return X
+}
+
+func mnaPrintMatrices(H [][]float64, B []float64, X []float64) {
+	for i, l := range H {
+		for j, v := range l {
+			fmt.Printf("H(%d,%d) = %f\n", i, j, v)
+		}
+	}
+
+	for i, v := range B {
+		fmt.Printf("B(%d) = %f\n", i, v)
+	}
+
+	//for i, v := range X {
+	//	fmt.Printf("X(%d) = %f\n", i, v)
+	//}
+}
+
+func printMatrix(matrix [][]float64) {
+	for _, l := range matrix {
+		fmt.Printf("[")
+		for j, k := range l {
+			fmt.Printf("%.3f", k)
+			if j != len(l)-1 {
+				fmt.Printf(",")
+			}
+		}
+		fmt.Printf("]\n")
 	}
 }
